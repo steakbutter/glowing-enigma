3d32d7f2c43266b09a6951a5bbc61c6fc778df6a
steakbutter <carlosgzzperales@gmail.com>
2024-07-23 13:39:23 -0600

    Added code to the User model file with requirements specified

---
+++ models/User.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+\@.+\..+/, 'Please fill a valid email address']
+        },
+        thoughts: {
+            type: Schema.Types.ObjectId,
+            ref: 'thought'
+        },
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        }]
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+      }
+);
+
+// Virtual property
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
