a790108d1cf5700d7edb9a2f96b75d4243c7ee87
steakbutter <carlosgzzperales@gmail.com>
2024-07-23 21:45:26 -0600

    Fix some typos to run the server correctly

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,4 +12,33 @@ module.exports = {
         }
     },
 
+    // Get a single User
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+
+            if (!user) {
+                return res.status(400).json({ message: 'No user found with that id'});
+            }
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json(error);
+        }
+    },
+
+    // Create a user
+    async createUser(req, res) {
+        return res.status(200)
+    },
+
+    // Update User
+    async updateUser(req, res) {
+        return res.status(200)
+    },
+
+    // Delete User
+    async deleteUser(req, res) {
+        return res.status(200)
+    },
+
 }
