eed812b30315df25f2109d273a776b454220f7e4
steakbutter <carlosgzzperales@gmail.com>
2024-07-25 21:46:10 -0600

    fix the error on getting a single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,6 +21,7 @@ module.exports = {
             if (!user) {
                 return res.status(400).json({ message: 'No user found with that id'});
             }
+            res.status(200).json(user);
         } catch (error) {
             console.log(error);
             return res.status(500).json(error);
